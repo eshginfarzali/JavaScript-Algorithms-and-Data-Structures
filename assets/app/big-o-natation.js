611c78ecb03ee1805723b632d113e864a989fe03
Eshgin Farzaliyev <eshqinferzeliyev@gmail.com>
2023-03-09 13:47:29 +0400

    summation

---
+++ assets/app/big-o-natation.js
@@ -0,0 +1,13 @@
+  function summation(n){
+    let sum =0;
+    
+    for(let i=1; i<=n; i++){
+        sum+=i
+        // console.log(sum );
+    }
+    return sum;
+    
+  }
+  
+  summation(10)
+ 
